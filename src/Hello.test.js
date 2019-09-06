d6b6a9fd79cda6da5e1c3fdc854a22325da44f6f
neyb <noo.blaster@gmail.com>
2019-09-06 15:36:09 +0200

    add simple component with a test

---
+++ src/Hello.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from '@testing-library/react'
+
+import Hello from "./Hello";
+
+describe("hello component", () => {
+
+  test("importing component", () => {
+    expect(Hello).toBeTruthy()
+  });
+
+  test("can render for name world", () => {
+    let rendered = render(<Hello name="world"/>);
+    let selected = rendered.getByText("hello world");
+    expect(selected).toBeTruthy()
+  })
+});
--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "/custom.css";
 import ReactDOM from "react-dom";
+import Hello from "/Hello";
 
-const App = () => (<div>
-  hello
-</div>);
+const App = () => <div><Hello name="world"/></div>
 
 ReactDOM.render(<App/>, document.getElementById("root"));
 
