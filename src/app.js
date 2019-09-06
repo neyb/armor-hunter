10c061c07053a76e4a65e051b5aaa1e479312bd0
neyb <noo.blaster@gmail.com>
2019-09-06 14:43:32 +0200

    parcel start

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "/custom.css";
+import ReactDOM from "react-dom";
+
+const App = () => (<div>
+  hello
+</div>);
+
+ReactDOM.render(<App/>, document.getElementById("root"));
+
