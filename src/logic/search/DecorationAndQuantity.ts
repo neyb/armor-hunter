be616b154225402079358a09270e129317756465
neyb <noo.blaster@gmail.com>
2020-02-28 20:31:52 +0100

    remove unused class

--- src/logic/search/DecorationAndQuantity.ts
+++
@@ -1,9 +0,0 @@
-import {is, hash, ValueObject} from "immutable"
-import {Decoration} from "/logic/search/decoration"
-
-export class DecorationAndQuantity implements ValueObject {
-  constructor(readonly decoration: Decoration, readonly qty: number) {}
-
-  hashCode = () => hash(this.decoration) + this.qty
-  equals = (other: DecorationAndQuantity) => is(this.decoration, other.decoration) && is(this.qty, other.qty)
-}
