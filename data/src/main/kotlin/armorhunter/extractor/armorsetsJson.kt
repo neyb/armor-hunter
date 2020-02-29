93fe70a267b5f5e1ad0a682475249a52b3519802
neyb <noo.blaster@gmail.com>
2020-02-29 08:28:11 +0100

    add skillset support

--- data/src/main/kotlin/armorhunter/extractor/armorsetsJson.kt
+++ data/src/main/kotlin/armorhunter/extractor/armorsetsJson.kt
@@ -12,10 +12,14 @@ fun ArmorSet.json() = with(parts) {
                  with(defenses) {
                      DefensesJson(with(physical) { PhysicalDefenseJson(base, max, augmented) },
                                   fire, water, thunder, ice, dragon)
-                 })
+                 },
+                skillSets.map(SkillSet::json)
+    )
 }
 
 private fun Armor.json() = ArmorJson(slots.map { it.size }, skills.associateBy({ it.skill.name }, { it.level }))
+
+private fun SkillSet.json() = SetSkill(skill.name, nbParts)
 
 
 @Serializable data class ArmorSetJson(val rarity: Int,
@@ -24,7 +28,8 @@ private fun Armor.json() = ArmorJson(slots.map { it.size }, skills.associateBy({
                                       val arms: ArmorJson?,
                                       val waist: ArmorJson?,
                                       val legs: ArmorJson?,
-                                      val defenses: DefensesJson)
+                                      val defenses: DefensesJson,
+                                      val setSkill: List<SetSkill>)
 
 @Serializable data class DefensesJson(val physical: PhysicalDefenseJson,
                                       val fire: Int,
@@ -36,3 +41,5 @@ private fun Armor.json() = ArmorJson(slots.map { it.size }, skills.associateBy({
 @Serializable data class PhysicalDefenseJson(val base: Int, val max: Int, val augmented: Int)
 
 @Serializable data class ArmorJson(val slots: List<Int>, val skills: Map<String, Int>)
+
+@Serializable data class SetSkill(val skill:String, val nbParts:Int)
