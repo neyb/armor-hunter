3055c8244c82295394cf46bed659ea759dcc326c
neyb <noo.blaster@gmail.com>
2020-03-06 22:47:10 +0100

    add redux persist

--- src/logic/store.ts
+++ src/logic/store.ts
@@ -1,6 +1,10 @@
-import {createStore, combineReducers} from "redux"
+import {combineReducers, createStore} from "redux"
 import {reducer as builder, State as Builder} from "./builder/store"
+import {persistReducer, persistStore} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 export type RootState = {builder: Builder}
 
-export default createStore(combineReducers({builder}))
+export const store = createStore(persistReducer({key: "root", storage}, combineReducers({builder})))
+
+export const persistor = persistStore(store)
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -2,13 +2,16 @@ import React from "react"
 import Builds from "./builds"
 import Builder from "./builder"
 import {Provider} from "react-redux"
-import store from "/logic/store"
+import {store, persistor} from "/logic/store"
+import {PersistGate} from "redux-persist/integration/react"
 
 export const App = () => (
   <Provider store={store}>
-    <div className="row">
-      <Builds />
-      <Builder />
-    </div>
+    <PersistGate persistor={persistor}>
+      <div className="row">
+        <Builds />
+        <Builder />
+      </div>
+    </PersistGate>
   </Provider>
 )
