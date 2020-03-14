71b866a4235e483f0d88d2a4fa9bd8e417592db7
neyb <noo.blaster@gmail.com>
2020-03-14 09:10:08 +0100

    feat: show results as json

--- src/ui/builder/Results.tsx
+++ src/ui/builder/Results.tsx
@@ -1,5 +1,16 @@
 import React from "react"
+import {useSelector} from "react-redux"
+import {RootState} from "../../logic/store"
 
 export default function Results({}) {
-  return <div></div>
+  const results = useSelector((state: RootState) => state.builder.results)
+
+  return (
+    <div>
+      <div>nbResults:{results.length}</div>
+      {results.map(result => (
+        <div key={result.id}>{JSON.stringify(result)}</div>
+      ))}
+    </div>
+  )
 }
