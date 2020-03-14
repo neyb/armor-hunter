13915cdca01eed5e0ff2f299fcb7a399d8d1ef23
neyb <noo.blaster@gmail.com>
2020-03-14 15:49:36 +0100

    refacto: update merge to be not only for state

--- src/lib/merge.ts
+++ src/lib/merge.ts
@@ -1,9 +1,9 @@
 import {isArray, mergeWith, MergeWithCustomizer} from "lodash"
 
 const customizer: MergeWithCustomizer = (_, src) => (isArray(src) ? src : undefined)
-export const merge = <State>(state: State, ...others: RecursivePartial<State>[]): State =>
-  others.reduce((acc, partial) => mergeWith(acc, partial, customizer), mergeWith({}, state, customizer))
+export const merge = <T>(first: T, ...others: RecursivePartial<T>[]): T =>
+  others.reduce((acc, partial) => mergeWith(acc, partial, customizer), mergeWith({}, first, customizer))
 
-type RecursivePartial<T> = T extends primitive ? T : RecursivePartialObject<T>
+export type RecursivePartial<T> = T extends primitive ? T : RecursivePartialObject<T>
 type primitive = string | number | boolean | undefined | null
 type RecursivePartialObject<T> = {[P in keyof T]?: RecursivePartial<T[P]>}
